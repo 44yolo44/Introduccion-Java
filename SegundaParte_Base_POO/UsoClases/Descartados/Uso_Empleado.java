810c3caa0cf083a68e95613f53f014636f4103dc
Mauricio Fabian Farro Izquierdo <106629542+Mfarro1522@users.noreply.github.com>
2025-03-15 14:07:58 -0500

    Update Uso_Empleado.java

--- SegundaParte_Base_POO/UsoClases/Descartados/Uso_Empleado.java
+++ SegundaParte_Base_POO/UsoClases/Descartados/Uso_Empleado.java
@@ -5,51 +5,44 @@ import java.util.*;
 public class Uso_Empleado {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		/*Empleado empleado1 = new Empleado("Pepito quispe " , 30000 , 1990 , 12 , 17);
-		
-		Empleado empleado2 = new Empleado("Ana lopez " , 800 , 2015 , 2 , 13);
-		
-		Empleado empleado3 = new Empleado("lamine mamani" , 105000 , 2024 , 4 , 16);
 
-		empleado1.subeSueldo(5);
-		empleado2.subeSueldo(5);
-		empleado3.subeSueldo(5);
 		
-		System.out.println("Nombre : "+empleado1.getnombre()+" Sueldo: "+empleado1.getsueldo()+
-							" Fecha de alta: "+empleado1.getfechaContrato());
+		Jefatura jefe_RRHH =  new Jefatura ("Mauricio" , 200000 , 2026 , 5 , 15 ) ;
+		jefe_RRHH.establece_incentivo(62560);
 		
-		System.out.println("Nombre : "+empleado2.getnombre()+" Sueldo: "+empleado2.getsueldo()+
-				" Fecha de alta: "+empleado2.getfechaContrato());
 		
-		System.out.println("Nombre : "+empleado3.getnombre()+" Sueldo: "+empleado3.getsueldo()+
-				" Fecha de alta: "+empleado3.getfechaContrato());*/
 		 
-		Empleado[] MisEmpleados = new Empleado[3] ;
+		Empleado[] MisEmpleados = new Empleado[6] ;
 		
 		MisEmpleados[0] = new Empleado("Pepito quispe " , 30000 , 1990 , 12 , 17) ;
 		MisEmpleados[1] = new Empleado("Ana lopez " , 800 , 2015 , 2 , 13);
 		MisEmpleados[2] = new Empleado("lamine mamani" , 105000 , 2024 , 4 , 16);
 		
-		/*1for (int i = 0 ; i<3 ; i++) {
-			MisEmpleados[i].subeSueldo(5);
-		}
-		*/
+		//de otro constructor 
+		
+		MisEmpleados[3] = new Empleado("Pedri Quizpe"); 
+		
+		MisEmpleados [4] = jefe_RRHH ; //principio de sustitucion 
+		
+		MisEmpleados [5] = new Jefatura("valeria", 150000, 2028, 3, 15);
+		
+		
+		
+		
 		for (Empleado e : MisEmpleados ) {
 			e.subeSueldo(5);
 		}
 		
-		/*for(int i = 0 ; i<3 ; i++) {
-			System.out.println("Nombre : "+MisEmpleados[i].getnombre()+"Sueldo : "+
-			MisEmpleados[i].getsueldo()+" Fecha de alta: "+MisEmpleados[i].getfechaContrato());
-			
-		}
-		*/
+		
 		
 		for (Empleado f : MisEmpleados) {
-			System.out.println("Nombre : "+f.getnombre()+"Sueldo : "+
+			System.out.println("Nombre : "+f.getnombre()+" Sueldo : "+
 				f.getsueldo()+" Fecha de alta: "+f.getfechaContrato());
+			/*
+			en f.getsueldo actua en todos como get sueldo del que pertenerce 
+			que es mis empleados que es un array tipo empleados 
+			en f.getsueldo al lleagr al 4 actua como getsueldo pero de jefatura
+			*/
 		}
 		
 	}
@@ -69,10 +62,25 @@ class Empleado {
 		
 		altaContratado = calendario.getTime();
 		
+		// Asignar id al momento de crear el objeto
+		id = idSiguiente;
+		idSiguiente++;
 	}
+	
+	//se puede tener varios constructores (no tiene limite) pero debe variar en orden , tipo o cantidad de parametros
+	//this lo que hqace es llama al otro constructor llamado igual con el q coicida los parametros 
+	public Empleado(String nom) {
+		
+		this(nom , 30000 , 2025 , 01 , 01);
+		
+	}
+	
+		
+	
 	//getters
 	public String getnombre(){
-		return nombre;
+		// Ya no asignamos el id aquí
+		return nombre + " ID: "+id;
 	}
 	
 	public double getsueldo() {
@@ -98,5 +106,38 @@ class Empleado {
 	
 	private Date altaContratado;
 	
+    // Variable de clase (por el static)
+    private static int idSiguiente =1;
 	
+    private int id; // ID del empleado
+    
+}
+	
+
+
+
+
+class Jefatura extends Empleado{
+	
+	public Jefatura (String nom , double sue , int agno , int mes , int dia ) {
+		
+		super(nom , sue , agno , mes , dia);
+		
+	}
+	
+	//setter incentivo 
+	public void establece_incentivo (double b) {
+		incentivo=b;
+	}
+	
+	//getter incentivo (machaco el getsueldo de la clase empleado)
+	public double getsueldo () {
+		
+		double SueldoJefe = super.getsueldo();
+		
+		return SueldoJefe + incentivo ;
+	}
+	
+	
+	private double incentivo ; //varaibles  , campos de clase van al principio o al final 
 }
